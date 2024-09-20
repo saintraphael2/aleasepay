42582682c30b71b5d825136b61ae7d9404b29093
Kpoti Kokou AKOGO <kpoti.akogo@africanlease.com>
2024-09-20 09:38:01 +0000

    saveof20092024A

--- app/Http/Controllers/MouvementController.php
+++ app/Http/Controllers/MouvementController.php
@@ -45,7 +45,7 @@ class MouvementController extends AppBaseController
             $deb=Carbon::parse($request->deb);
         }
         $compte=($request->compte !== null)?$request->compte:$comptes[0]->compte;
-        $mouvements=Mouvement::where('ECRCPT_NUMCPTE', $compte)->whereBetween('LOT_DATE', [$deb,$fin])->get();
+        $mouvements=Mouvement::where('ECRCPT_NUMCPTE', $compte)->whereBetween('LOT_DATE', [$deb,$fin])->orderby('LOT_DATE','desc')->get();
        // dd($mouvements);
         return view('mouvements.index')->with(['mouvements'=>$mouvements,'deb'=>$deb,'fin'=>$fin,'comptes'=>$comptes,'compte'=> $compte]);
     }
