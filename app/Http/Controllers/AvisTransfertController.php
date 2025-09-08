e2af2d4a22cae0584412fc225b19f93e84170c66
saintraphael2 <saintraphael2@gmail.com>
2025-09-08 17:30:05 +0000

    saveof08092025A

--- app/Http/Controllers/AvisTransfertController.php
+++ app/Http/Controllers/AvisTransfertController.php
@@ -152,6 +152,9 @@ class AvisTransfertController extends AppBaseController
     if(preg_match('/\breglement fa\b/', $motif)){
         $motif=str_replace('reglement fa','Règlement Facture',$avis[0]['ecrcptLibcomp']);
     }
+    if(preg_match('/\breglement facture\b/', $motif)){
+        $motif=str_replace('reglement fa','Règlement Facture',$avis[0]['ecrcptLibcomp']);
+    }
     $fpdf->Rect(10,150,45,50);$fpdf->SetXY(12,157);$fpdf->MultiCell(40, 10, utf8_decode($motif), 0, 'L');// $fpdf->Cell(45, 10, utf8_decode('FRAIS SUR TRANSFERT'),0,1,'C',false);
     $fpdf->Rect(55,150,95,50);
     $y=157;
